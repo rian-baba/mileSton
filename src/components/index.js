38c99f358dac895f9b5a3321fe46138ec426f766
malikriannakeez@gmail.com <malikriannakeez@gmail.com>
2025-09-29 00:37:23 +0500

    build logs improve

--- src/components/index.js
+++ src/components/index.js
@@ -1,30 +1,18 @@
-import Select from "./Select";
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import Container from "./Container/Container";
-import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn";
-import RTE from "./RTE";
-import Signup from "./Signup";
-import Login from "./Login";
+import AuthLayout from "./AuthLayout";
 import Button from "./Button";
+import Container from "./Container/Container";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import LogoutBtn from "./Header/Logoutbtn";
+import Input from "./Input";
+import Login from "./Login";
+import Logo from "./Logo";
 import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
-import AuthLayout from "./AuthLayout";
-import Input from "./Input";
+import RTE from "./RTE";
+import Select from "./Select";
+import Signup from "./Signup";
 export {
-    Header,
-    Footer,
-    Container,
-    Logo,
-    LogoutBtn,
-    RTE,
-    Signup,
-    Login,
-    Button,
-    PostForm,
-    PostCard,
-    AuthLayout, 
-    Input,
-    Select,
-}
+    AuthLayout, Button, Container, Footer, Header, Input, Login, Logo,
+    LogoutBtn, PostCard, PostForm, RTE, Select, Signup
+};
